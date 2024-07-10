1404fcbd2e9177b2663891a5d55cc3791b1cd70d
remi.kristelijn <remi.kristelijn@theapsgroup.com>
2024-07-10 17:18:30 +0200

    chore: save work

--- src/app/(auth)/callback/route.ts
+++ src/app/(auth)/callback/route.ts
@@ -12,6 +12,7 @@ export async function GET(request: Request) {
 
   console.log('callback/route.ts', { code, next, searchParams, origin })
 
+<<<<<<< Updated upstream
   if (code != null) {
     console.log('callback/route.ts', 'code found in query params', {code})
     const cookieStore = cookies()
@@ -51,9 +52,41 @@ export async function GET(request: Request) {
       console.log('callback/route.ts', origin)
 
       return NextResponse.redirect(`${origin}/error`)
+=======
+  if (!code) {
+    console.log('callback/route.ts', 'code not found in query params...')
+    return NextResponse.redirect(`${origin}/error`)
+  }
+
+  console.log('callback/route.ts', 'code found in query params')
+  const cookieStore = cookies()
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+>>>>>>> Stashed changes
     }
+  )
+  const { error } = await supabase.auth.exchangeCodeForSession(code)
+  if (error == null) {
+    console.log('Redirecting to main page')
+    console.log(next)
+    return NextResponse.redirect(`${origin}${next}`)
   } else {
-    console.error('callback/route.ts', 'No code found in query params')
-    return NextResponse.redirect(`${origin}/`)
+    return NextResponse.redirect(`${origin}/error`)
   }
 }
